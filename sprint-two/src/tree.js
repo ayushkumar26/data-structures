59f4120bf26c3d8831c8b7b8cf9673ae6173b5f9
ayushkumar26 <ayushkumar26@gmail.com>
2017-10-12 13:23:49 -0700

    tree implemented, tests passing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  console.log('Tree value:', this.value);
+  console.log('Target:', target);
+  var contained = false;
+  if (this.value === target) {
+    console.log('Was here');
+    contained = true;
+  } else { 
+    for (var i = 0; i < this.children.length; i++) {
+      var isContained = this.children[i].contains(target);
+      if (isContained) {
+        contained = true;
+      }
+    }
+  }
+
+
+  //var targetFound = false;
+  // _.each(this.children, function(child) {
+  //   targetFound = child.contains(target);
+  //   //return targetFound;
+  // });
+  //return targetFound;
+  return contained;
 };
 
 
