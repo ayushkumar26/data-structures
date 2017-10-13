26d3441a07b7f1953b276b4a85d032e83e2b1f66
ayushkumar26 <ayushkumar26@gmail.com>
2017-10-12 22:14:50 -0700

    graph fully implemented, need complexity analysis

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -13,7 +13,6 @@ Graph.prototype.addNode = function(node) {
   this.nodeList[node] = node;
   this.edgeList[node] = [];
   
-  //console.log('this.nodeList:', this.nodeList);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -23,10 +22,6 @@ Graph.prototype.contains = function(node) {
   }
   return false;
 
-  // if (this.nodeList.includes(node)) {
-  //   return true;
-  // } 
-  // return false;
 };
 
 // Removes a node from the graph.
@@ -34,9 +29,6 @@ Graph.prototype.removeNode = function(node) {
   console.log('node:', node);
   delete this.nodeList[node];
   
-  //var index = this.edgeList[node] ? this.edgeList[node].indexOf(node) : -1;
-  //this.edgeList[index].splice(0, this.edgeList[index].length);
-
   for (var i = 0; i < this.edgeList.length; i++) {
     var index = this.edgeList[i] ? this.edgeList[i].indexOf(node) : -1;
     console.log('Other lists before splice:', this.edgeList[i]);
@@ -45,15 +37,7 @@ Graph.prototype.removeNode = function(node) {
       this.edgeList[i].splice(index, 1);
     }
     console.log('Other lists after splice:', this.edgeList[i]);
-    //if (this.edgeList[i] !== undefined) {
-    // for (var j = 0; j < this.edgeList[i].length; j++) {
-    //   if (this.edgeList[i][j] === node) {
-    //     this.edgeList[i].splice(this.edgeList[i][j], 1);
-    //     console.log('Other edgeLists:', this.edgeList[i]);
-    //   }
-    // }    
-     // this.edgeList[i].splice(0, this.edgeList[i].length);
-    //}
+
   }
   this.edgeList.splice(node, 1);
   console.log('Edge list of node:', this.edgeList[node]);
@@ -74,18 +58,7 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // console.log('fromNode:', fromNode);
-  // console.log('toNode:', toNode);
-  // //this.nodeList.edge = fromNode;
-  // this.nodeList['fromNode'] = fromNode;
-  // this.nodeList.toNode = toNode;
 
-
-   // {1: {1: 2}}
-   // {1: 1, to: 1, from: 2}
-// {1: 1. edge: 2}
-   //{1 : edges}
-  //console.log(this.edgeList);  
   this.edgeList[fromNode].push(toNode);
   this.edgeList[toNode].push(fromNode);
   //console.log('fromNode edgeList:', this.edgeList[fromNode]);
@@ -95,8 +68,7 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  //console.log('edgeList of fromNode:', this.edgeList[fromNode]);
-  //console.log('edgeList of toNode:', this.edgeList[toNode]);
+
 
   var indexFrom = this.edgeList[fromNode] ? this.edgeList[fromNode].indexOf(toNode) : -1; 
   var indexTo = this.edgeList[toNode] ? this.edgeList[toNode].indexOf(fromNode) : -1;
@@ -109,21 +81,13 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
     this.edgeList[toNode].splice(indexTo, 1);
   }
 
-  // for (var i = 0; i < this.edgeList[fromNode]; i++) {
-  //   if (this.edgeList[fromNode][i] === toNode) {
-  //     this.edgeList[fromNode].splice(this.edgeList[fromNode][i], 1);
-  //   } 
-  // }
-  // for (var i = 0; i < this.edgeList[toNode]; i++) {
-  //   if (this.edgeList[toNode][i] === fromNode) {
-  //     this.edgeList[toNode].splice(this.edgeList[toNode][i], 1);
-  //   } 
-  // }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-
+  for (var key in this.nodeList) {
+    cb(this.nodeList[key]);
+  }
 };
 
 /*
